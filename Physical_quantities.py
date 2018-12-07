5c65ad65bbd59381e694c2e818e2dde2f62a6969
benesteves <44998529+benesteves@users.noreply.github.com>
2018-12-07 15:30:27 +0100

    Definitive file

--- Physical_quantities.py
+++ Physical_quantities.py
@@ -2,24 +2,35 @@
 import numpy as np
 
 "Charge of the electron"
-q = 1  
+q = -1  
 
 "Mass of the electron"             
 m = 1 
 
 "Amplitudes of the incident function"                 
-E1=1 
+E0=1 
 E2=1
-E3=1  
+E3=1
+E0_env=0.0001 
   
 "Period of the oscillation"               
 T=1 
   
 "Proper frequency"                  
 omega=2*np.pi/T
+omega_env=0.05
+
+T_env=(2*np.pi)/omega_env
+
+"Duration of the pulse"
+tau=800
 
 "Phase"	         
 phi1=0													
 phi2=0	
 phi3=0	
 
+
+#Numerical constants
+
+Number_of_points=100000
