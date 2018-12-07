511bb7ef91481761072a28dc9bc792448ea301fb
martinluttmann <44998370+martinluttmann@users.noreply.github.com>
2018-12-07 23:23:18 +0100

    Add files via upload
    Definitive files

--- Cranknicolsoneasy.py
+++ Cranknicolsoneasy.py
@@ -2,14 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov 13 15:09:38 2018
-
 @author: martin.luttmann
 """
 
 from matplotlib import pyplot as plt
-from math import *
-
-
 import scipy
 import numpy as np
 import scipy.sparse
@@ -17,9 +13,9 @@ import scipy.linalg as la
 
 
 
-
-class CrankNicolson2:
-    """A class that solves Schrodingers equation"""
+########### A class that solves Schrodingers equation ###########
+class CrankNicolson:
+    
     
     def set_grid(self, x_min, x_max, n_x, t_min, t_max, n_t):
 
@@ -33,71 +29,35 @@ class CrankNicolson2:
         
         self.f =  f
     
+    
+    # Returns the 1D laplacian of the wavefunction 
     def get_laplacian(self, psi):
-        """return the 1D laplacian of the wavefunction"""
+        
         L = np.zeros(self.n_x)*1j
         for i in range(1,self.n_x-1):
             L[i] = (psi[i+1]+psi[i-1]-2*psi[i])
         return L/self.delta_x**2
 
-#get next psi
-    def solve(self, psi_init, V_matrix, boundary_conditions):
+
+    def solve(self, psi_init, V_matrix, E_matrix, boundary_conditions):
             
         sig = 1j* self.delta_t / (4.* self.delta_x**2)
         
-        #norm_psi=np.trapz(abs(psi_init)**2,self.x_pts)
-        #psi_init/=np.sqrt(norm_psi)
-        
-        
-        # Figure the data type
+      
         data_type = type(sig*psi_init[0])
         
-        self.psi_matrix = np.zeros([self.n_t, self.n_x], dtype=data_type)  #matrice des fonctions d'onde
-  
-
-
-        '''
-            
-        A = self._make_tridiag(sig, self.n_x, data_type)
-        B = self._make_tridiag(-sig,  self.n_x, data_type)
-        # Set boundary conditions
-        for b in [0,1]:
-            if boundary_conditions[b] == 'dirichlet':
-                    # u(x,t) = 0
-                A[-b,-b] = 1.0
-                A[-b,1-3*b] = 0.0
-                B[-b,-b] = 0.0
-                B[-b,1-3*b] = 0.0
-            elif boundary_conditions[b] == 'neumann':
-                    # u'(x,t) = 0
-                A[-b,1-3*b] = -2*sig
-                B[-b,1-3*b] = 2*sig
-
-            # Propagate
-        psi = psi_init
-        #print(B)
-        #print(sig)
-        inv_A=np.linalg.inv(A)
-        fpsi_1=self.f(psi,0)
         
-            
-        for n in range(self.n_t):
-            self.psi_matrix[n,:] = psi
-            #fpsi = self.f(psi,n)
-            fpsi_2=self.f(psi,n)
-            
-            psi = la.solve(A, B.dot(psi) - 1j*self.delta_t * (3/2*fpsi_2-1/2*fpsi_1))
-            #psi=inv_A.dot(B.dot(psi) - 1j*self.delta_t * (3/2*fpsi_2-1/2*fpsi_1))
-            fpsi_1=fpsi_2
-            
-            #psi = np.linalg.solve(A, B.dot(psi))
-            
-        '''
+        # Matrix of the wavefunctions at differents t: each column of psi_matrix is a wavefunction at fixed t
+        self.psi_matrix = np.zeros([self.n_t, self.n_x], dtype=data_type)  
   
+
+        # Calculating the matrices of the linear system 
         A = self._fillA_sp(sig, self.n_x, data_type)
         B = self._fillB_sp(sig, self.n_x, data_type)
             
-            # Set boundary conditions
+        
+        
+        # Setting boundary conditions
         for b in [0,1]:
             if boundary_conditions[b] == 'dirichlet':
                 # u(x,t) = 0
@@ -110,14 +70,14 @@ class CrankNicolson2:
                 A[2*b,1-3*b] = -2*sig
                 B[-b,1-3*b] = 2*sig
                 
-        # Propagate
+        ##### Propagating the solution #####
         psi = psi_init
         fpsi_1=self.f(psi,0)
         for n in range(self.n_t):
             self.psi_matrix[n,:] = psi
             Laplacian_psi = self.get_laplacian(psi)
             self.position[n] = scipy.integrate.simps(psi.conjugate()*self.x_pts*psi, dx = self.delta_x) #compute the average position at time n*delta_t
-            self.dipole_acceleration[n] = scipy.integrate.simps(psi.conjugate()*-1*np.gradient(V_matrix[n], self.delta_x)*psi, dx = self.delta_x) #compute the average dipole acceleration at time n*delta_t
+            self.dipole_acceleration[n] = scipy.integrate.simps(psi.conjugate()*-1*np.gradient(V_matrix[n], self.delta_x)*psi+E_matrix[n], dx = self.delta_x) #compute the average dipole acceleration at time n*delta_t
             
             fpsi_2=self.f(psi,n)
             
@@ -137,19 +97,9 @@ class CrankNicolson2:
     def get_second_psi(self):
         return self.psi_matrix[1,:].copy()
         
-    '''
-    
-    def _make_tridiag(self, sig, n, data_type):
-    
-        M = np.diagflat(np.full(n, (1+2*sig), dtype=data_type)) + \
-            np.diagflat(np.full(n-1, -(sig), dtype=data_type), 1) + \
-            np.diagflat(np.full(n-1, -(sig), dtype=data_type), -1)
 
-        return M
-    '''  
-    
+    ####### Returns a tridiagonal matrix in compact form ab[1+i-j,j]=a[i,j] #######
     def _fillA_sp(self, sig, n, data_type):
-        """Returns a tridiagonal matrix in compact form ab[1+i-j,j]=a[i,j]"""
         
         A = np.zeros([3,n], dtype=data_type) # A has three diagonals and size n
         A[0] = -sig # superdiagonal
@@ -157,8 +107,8 @@ class CrankNicolson2:
         A[2] = -sig # subdiagonal
         return A
 
+    ####### Returns a tridiagonal sparse matrix in csr-form #######
     def _fillB_sp(self, sig, n, data_type):
-        """Returns a tridiagonal sparse matrix in csr-form"""
         
         _o = np.ones(n, dtype=data_type)
         supdiag = (sig)*_o[:-1]
